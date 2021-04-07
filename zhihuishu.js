c552d64e19dd034d92a1e3041a28e09959ad7e3a
liulinboyi <814921718@qq.com>
2021-04-07 21:56:56 +0800

    feat: Modify zhihuishu

--- zhihuishu.js
+++ zhihuishu.js
@@ -4,6 +4,7 @@ setTimeout(function () {
     console.log("已进行静音和1.5倍加速");
 }, 3000);
 
+var title = document.title
 setInterval(function () {
 
     if ($('.el-dialog__header>div>h4').text() == "弹题测验") {
@@ -15,8 +16,9 @@ setInterval(function () {
     }
 
     if (
-        $(".current_play div b:nth-child(2)").hasClass('time_icofinish') ||
-        $(".current_play div b:nth-child(3)").hasClass('time_icofinish')
+        $(".currentTime").text() === $(".duration").text()
+        //         $(".current_play div b:nth-child(2)").hasClass('time_icofinish') ||
+        //         $(".current_play div b:nth-child(3)").hasClass('time_icofinish')
     ) {
         console.log("检测到视频观看完成，准备跳到下一节");
         $('.nextButton').click()
@@ -26,6 +28,10 @@ setInterval(function () {
             $('.speedTab15').click();
         }, 5000);
     }
-
+    let duration = $(".duration").text().split(":")[0] * 60 * 60 + $(".duration").text().split(":")[1] * 60 + $(".duration").text().split(":")[2]
+    let currentTime = $(".currentTime").text().split(":")[0] * 60 * 60 + $(".currentTime").text().split(":")[1] * 60 + $(".currentTime").text().split(":")[2]
+    let percent = (currentTime / duration).toFixed(2)
+    console.log(percent * 100 + "%")
+    document.title = `${percent*100}% ${title} ${$(".currentTime").text()}/${$(".duration").text()}`
 
 }, 3000)
